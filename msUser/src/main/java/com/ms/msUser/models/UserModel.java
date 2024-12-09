1583ba93d58c320a6feee61c52e974312ae343d2
Vtormacs <macskids98play@hotmail.com>
2024-12-09 15:19:27 -0300

    model do user

---
+++ msUser/src/main/java/com/ms/msUser/models/UserModel.java
@@ -0,0 +1,28 @@
+package com.ms.msUser.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_user")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserModel implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID userId;
+    private String name;
+    private String email;
+}
