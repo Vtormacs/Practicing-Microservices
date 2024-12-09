423fcfff1bed7ab9ad28b003ee7cab054b041641
Vtormacs <macskids98play@hotmail.com>
2024-12-09 15:33:54 -0300

    convertendo o dto para model

--- msUser/src/main/java/com/ms/msUser/controllers/UserController.java
+++ msUser/src/main/java/com/ms/msUser/controllers/UserController.java
@@ -1,7 +1,9 @@
 package com.ms.msUser.controllers;
 
+import com.ms.msUser.dtos.UserRecordDTO;
 import com.ms.msUser.models.UserModel;
 import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +15,8 @@ public class UserController {
 
     @PostMapping("/users")
     public ResponseEntity<UserModel> saveUser(@RequestBody @Valid UserRecordDTO userRecordDTO){
+        var userModel = new UserModel();
+        BeanUtils.copyProperties(userRecordDTO, userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body();
     }
 }
